b47c3fa0457612376aae58c533ffed31a6f69992
Jinatey <95699786+Jinatey@users.noreply.github.com>
2023-08-21 21:15:59 +0500

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 export default function home() {
   return (
-    <div className=" max-w-[45
-    0px]">
+    <div className=" mx-auto w-[500px]">
       <div>
-        <h1>Personal info</h1>
+        <h1  className=" text-lg font-bold">Personal info</h1>
         <p>Please provide your name, email adress and phone number.</p>
       </div>
 
@@ -21,6 +20,13 @@ export default function home() {
           </label>
          
       </form>
+
+      <div className=" flex justify-end mt-20">
+          <button className=" bg-blue-950 text-white p-3 px-5 rounded-md">
+            Next Step
+          </button>
+        </div>
+
     </div>
   );
 }
--- pages/plan.jsx
+++ pages/plan.jsx
@@ -2,38 +2,49 @@ import React from "react";
 
 export default function plan() {
   return (
-    <div
-      className=" mx-auto max-w-[45
-      0px]"
-    >
+    <div className=" mx-auto w-[500px]">
       <div className=" my-6">
-        <h1>Select your plan</h1>
+        <h1 className=" text-lg font-bold">Select your plan</h1>
         <p> you have the option of monthly or yearly billinng</p>
       </div>
 
       <div className=" grid grid-cols-3 gap-4">
-        <div className=" border-2 rounded border-black  p-2">
-          <div>img</div>
-          <div>
+        <div className=" border rounded border-slate-400  p-5">
+          <div className=" py-3">
+            <img src="/arcade.svg" alt="" />
+          </div>
+          <div className=" py-5">
             <p>Arcade</p>
-            <p>$9/mo</p>
+            <p className=" text-slate-500"> $9/mo</p>
           </div>
         </div>
-        <div className=" border-2 rounded border-black p-2 ">
-          <div>img</div>
-          <div>
+        <div className=" border rounded  border-slate-400 p-5 ">
+          <div className=" py-3">
+            <img src="/advanced.svg" alt="" />
+          </div>
+          <div className=" py-5">
             <p>Advanced</p>
-            <p>$12/mo</p>
+            <p className=" text-slate-500">$12/mo</p>
           </div>
         </div>
-        <div className=" border-2 rounded border-black p-2 ">
-          <div>img</div>
-          <div>
+        <div className=" border rounded  border-slate-400 p-5 ">
+          <div className=" py-3">
+            <img src="/pro.svg" alt="" />
+          </div>
+          <div className=" py-5">
             <p>Pro</p>
-            <p>$15/mo</p>
+            <p className=" text-slate-500">$15/mo</p>
           </div>
         </div>
+        
       </div>
+
+      <div className=" flex justify-between mt-20">
+          <button>Go Back</button>
+          <button className=" bg-blue-950 text-white p-3 px-5 rounded-md">
+            Next Step
+          </button>
+        </div>
     </div>
   );
 }
